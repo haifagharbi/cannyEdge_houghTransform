f53634afbb0fb754aadaa4eaf0cdd5bc60bee3cc
souravEMBS <souravba@alumni.upenn.edu>
2020-04-12 18:53:27 -0700

    First cut lane detection

---
+++ cannyEdge.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+image = mpimg.imread("C:\\Users\\Sourav\\Desktop\\selfDrivingCarProj\\cannyEdgeDetection\\exit-ramp.jpg")
+plt.imshow(image)
+
+
+import cv2  #bringing in OpenCV libraries
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #grayscale conversion
+plt.imshow(gray, cmap='gray')
+
+# Define a kernel size for Gaussian smoothing / blurring
+# Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
+kernel_size = 3
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+
+# Define parameters for Canny and run it
+# NOTE: if you try running this code you might want to change these!
+low_threshold = 70
+high_threshold = 100
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+# Display the image
+plt.imshow(edges, cmap='Greys_r')
+
+# Define a kernel size and apply Gaussian smoothing
+kernel_size = 5
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+# Define our parameters for Canny and apply
+low_threshold = 50
+high_threshold = 150
+masked_edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+
+import numpy as np
+# Define the Hough transform parameters
+# Make a blank the same size as our image to draw on
+rho = 1
+theta = np.pi/180
+threshold = 1
+min_line_length = 10
+max_line_gap = 1
+line_image = np.copy(image)*0 #creating a blank to draw lines on
+
+# Run Hough on edge detected image
+lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
+
+# Iterate over the output "lines" and draw lines on the blank
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+
+# Create a "color" binary image to combine with line image
+color_edges = np.dstack((masked_edges, masked_edges, masked_edges)) 
+
+# Draw the lines on the edge image
+combo = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
+plt.imshow(combo)
+
+plt.show(block=False)
+input('press <ENTER> to continue')
